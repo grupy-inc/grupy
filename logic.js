3a77d5039601e52b4ab80cc6394a2f308e712e5c
Duvanchi <cochize0@gmail.com>
2017-11-11 13:23:29 -0500

    Small Stuff

--- logic.js
+++ logic.js
@@ -21,9 +21,6 @@ function search() {
     // URL string to grab Top Song Info (Top 10)
     var SongQueryURL = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=" + searchBand + "&api_key=0cd512b53d58de3fd8a79d4be57a971c&format=json&limit=10";
 
-    var EventQueryURL = "https://app.swaggerhub.com/proxy?url=https%3A%2F%2Frest.bandsintown.com%2Fartists%2F" + searchBand + "%2Fevents%3Fapp_id%3DGroopy%26date%3D2017-11-30%252C2018-11-30&proxy-token=e6c96s1";
-    console.log(EventQueryURL);
-
     // Creates AJAX call for Band Info (JSON: BandQueryURL)
     $.ajax({
       url: BandQueryURL,
@@ -41,7 +38,7 @@ function search() {
     console.log(retrievedBandBio);
     console.log(retrievedBandImagePath);
 
-    $('.card-text').text(retrievedBandName);
+    // $('.card-text').text(retrievedBandName);
     $('#bandImage').text(retrievedBandName);
     $('#insert-bio').text(retrievedBandBio);
     $('#band-pic').attr('src', retrievedBandImagePath);
@@ -80,33 +77,6 @@ function search() {
     // console.log(retrievedSongName3);
     // console.log(retrievedSongName4);
     // console.log("--------------------------");
-    });
-
-    $.ajax({
-  url: EventQueryURL,
-  method: "GET"
-    }).done(function(response) {
-    // Variables for Event Info
-    var eventId  = response.id;
-    var eventLineup  = response[0].lineup;
-    var eventDate  = response[0].datetime;
-    var eventVenue  = response[0].venue.name;
-    var eventCity  = response[0].venue.city;
-    var eventState  = response[0].venue.region;
-    var eventOffer  = response[0].offers.type;
-    var eventAvailable  = response[0].offers.type;
-
-
-    console.log("Event ID: " + eventId);
-    console.log("Band Name: " + eventLineup);
-    console.log("Event Date: " + eventDate);
-    console.log("Event Venue: " + eventVenue);
-    console.log("Event City: " + eventCity);
-    console.log("Event State: " + eventState);
-    console.log("For Sale: " + eventOffer);
-    console.log("Status: " + eventAvailable);
-
-    console.log("--------------------------");
     });
 
 };
