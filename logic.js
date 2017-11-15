4dee6327f716e2472c28c2bf7847228b32b94d92
David Killian <dpkillian@icloud.com>
2017-11-15 13:30:58 -0500

    David reformatted logic.js & added comments

--- logic.js
+++ logic.js
@@ -1,75 +1,75 @@
+
+// **** LISTENER
+// Create a listener to trigger when "Search Icon" is clicked to clear the <div> 
+// and perform a search on the term in the search field
 $('#band-search').on('click', function() {
+    $('#song-list').empty();
+    $('#tour-list').empty();
+    $('#search-bar').trigger('reset');
+    search();
+});
+
+
+// **** LISTENER
+// Create a listener to trigger when "Enter" is pressed to clear the <div> 
+// and perform a search on the term in the search field
+$(document).keypress(function(e) {
+    if (e.which === 13) {
+        event.preventDefault();
         $('#song-list').empty();
         $('#tour-list').empty();
         $('#search-bar').trigger('reset');
         search();
-    });
-
-$(document).keypress(function(e) {
-       
-        if (e.which === 13) {
-            event.preventDefault();
-            $('#song-list').empty();
-            $('#tour-list').empty();
-            $('#search-bar').trigger('reset');
-            search();
-            console.log()
-        };
-    });
-
-    var config = {
-      apiKey: "AIzaSyBhkcmUfR4hH2ktzPqtZ5RrQlr1pxlJBVE",
-      authDomain: "groopy-ee480.firebaseapp.com",
-      databaseURL: "https://groopy-ee480.firebaseio.com",
-      projectId: "groopy-ee480",
-      storageBucket: "groopy-ee480.appspot.com",
-      messagingSenderId: "226298723960"
+        console.log()
     };
+});
+
+
+// Initialize Firebase (database)
+var config = {
+    apiKey: "AIzaSyBhkcmUfR4hH2ktzPqtZ5RrQlr1pxlJBVE",
+    authDomain: "groopy-ee480.firebaseapp.com",
+    databaseURL: "https://groopy-ee480.firebaseio.com",
+    projectId: "groopy-ee480",
+    storageBucket: "groopy-ee480.appspot.com",
+    messagingSenderId: "226298723960"
+};
        
+firebase.initializeApp(config);
 
-        firebase.initializeApp(config);
+// Create an instance of the database, called "database"       
+var database = firebase.database();
 
-        var database = firebase.database();
+// **** LISTENER - FIREBASE
+// Create a listener for the Firebase database; so when a 
+// child element is added, it will grab a snapshot of each 
+// object in the database that was created and store it in "sv"
+database.ref().on("child_added", function(childSnapshot) {
+    var sv = childSnapshot.val();
 
-        // **** LISTENER
-        // Create a listener for the Firebase database; so when a 
-        // child element is added, it will grab a snapshot of each 
-        // object in the database that was created and store it in "sv"
-        database.ref().on("child_added", function(childSnapshot) {
-        var sv = childSnapshot.val();
-        $('#saved-band1').text(sv.dband);
-
-        // Add relevent value from the object (sv.bandName) to button id <saved-band#>
-        // *** ADD HTML CODE HERE (sv.dband)
-
-          // If any errors are experienced, log them to console.
-        }, function(errorObject) {
-          console.log("The read failed: " + errorObject.code);
-        });
-
-        // **** LISTENER
-        // Create a listner for the "SAVE BAND TO FAVROITES" button that grabs
-        // relevent form values and writes them to the database
-        // $("#saveBand").on("click", function(){
-        //     event.preventDefault();
-        //     var saveToFavorites = retrievedBandName
+    // Write each element in the database (band name) to the saved band button list
+    $('#saved-band1').text(sv.dband);
 
 
-        //     database.ref().push({
-        //       dband: saveToFavorites,
-        //       dateAdded: firebase.database.ServerValue.TIMESTAMP
-        //     });
+    // If any errors are experienced, log them to console.
+    }, function(errorObject) {
+        console.log("The read failed: " + errorObject.code);
+});
 
-        // });
 
+// Function to query API's (Ticketmaster and last.fm)
 function search() {
-    // searchBand is entered in the search field in the DOM
+
+    // searchBand value is grabbed from the search field in the DOM
     var searchBand = $('#search-bar').val();
+
     // URL string to grab Band Info (Name, Image, BIO)
     var BandQueryURL = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + searchBand + "&api_key=0cd512b53d58de3fd8a79d4be57a971c&format=json&limit=1";
-    // URL string to grab Top Song Info (Top 10)
+
+    // URL string to grab Top Song Info (Top 10 Songs)
     var SongQueryURL = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=" + searchBand + "&api_key=0cd512b53d58de3fd8a79d4be57a971c&format=json&limit=10";
-    // URL string to grab Event Info (All)
+
+    // URL string to grab Event Info (Date, Time, Location, Event Name)
     var startDateTime = "&startDateTime=2017-11-20T00:00:00Z";
     var endDateTime   = "&endDateTime=2018-05-20T00:00:00Z";
     var countryCode   = "&countryCode=US";    
@@ -80,81 +80,93 @@ function search() {
       url: BandQueryURL,
       method: "GET"
     }).done(function(response) {
-    // Variables for BandName, BandImagePath and BandBio
-    var retrievedBandName  = response.artist.name;
-    // var retrievedBandImageSize = response.artist.image[3].size;
-    var retrievedBandBio = response.artist.bio.summary;
-    var retrievedBandImagePath = response.artist.image[3]["#text"];
-    // Console.log for BandName, BandImagePath and BandBio
-    console.log(retrievedBandName);
-    // console.log(retrievedBandImageSize);
-    console.log("--------------------------");
-    console.log(retrievedBandBio);
-    console.log(retrievedBandImagePath);
 
-    // $('.card-text').text(retrievedBandName);
-    $('#bandImage').text(retrievedBandName);
-    $('#insert-bio').text(retrievedBandBio);
-    $('#actualBio').attr("<a href=>");
-    $('#band-pic').attr('src', retrievedBandImagePath);
+        // Variables for BandName, BandImagePath and BandBio
+        var retrievedBandName  = response.artist.name;
 
-    $("#saveBand").on("click", function(){
+        // var retrievedBandImageSize = response.artist.image[3].size;
+        var retrievedBandBio = response.artist.bio.summary;
+        var retrievedBandImagePath = response.artist.image[3]["#text"];
+
+        // console.log for BandName, BandImagePath and BandBio
+        console.log(retrievedBandName);
+
+        // console.log(retrievedBandImageSize);
+        console.log("--------------------------");
+        console.log(retrievedBandBio);
+        console.log(retrievedBandImagePath);
+
+        // $('.card-text').text(retrievedBandName);
+        $('#bandImage').text(retrievedBandName);
+        $('#insert-bio').text(retrievedBandBio);
+        $('#actualBio').attr("<a href=>");
+        $('#band-pic').attr('src', retrievedBandImagePath);
+
+
+
+
+        // **** LISTENER
+        // Create a listner for the "SAVE BAND TO FAVORITES" button that grabs
+        // relevent form values and writes them to the database
+        $("#saveBand").on("click", function(){
             event.preventDefault();
             var saveToFavorites = retrievedBandName
 
-
+            // Write band name to Firebase
             database.ref().push({
               dband: saveToFavorites,
               dateAdded: firebase.database.ServerValue.TIMESTAMP
             });
-
         });
-
     });
+
+
+
+
     // Creates AJAX call for Song Info (JSON: SongQueryURL)
     // Currently working on displaying top tracks - Luke
     $.ajax({
-      url: SongQueryURL,
-      method: "GET"
+        url: SongQueryURL,
+        method: "GET"
     }).done(function(response) {
 
-    var topSongs = []
-    var returnedSongs = response.toptracks.track
-    var stream = parseInt(response.toptracks.track.streamable)
-    var songURL = response.toptracks.track.url
-    topSongs.push(returnedSongs);
-    console.log(topSongs);
-    console.log(returnedSongs);
-    console.log(songURL);
+        var topSongs = []
+        var returnedSongs = response.toptracks.track
+        var stream = parseInt(response.toptracks.track.streamable)
+        var songURL = response.toptracks.track.url
+        topSongs.push(returnedSongs);
+        console.log(topSongs);
+        console.log(returnedSongs);
+        console.log(songURL);
     
 
-    for (i = 0; i < returnedSongs.length; i++) {
-        var link = "<a" + " target=" + "'_blank'" + "href=" + returnedSongs[i].url + ">";
-        var songNames = returnedSongs[i].name;
-        var songList = "<li>" + link + songNames;
-        // console.log(returnedSongs[i].url);
+        for (i = 0; i < returnedSongs.length; i++) {
+            var link = "<a" + " target=" + "'_blank'" + "href=" + returnedSongs[i].url + ">";
+            var songNames = returnedSongs[i].name;
+            var songList = "<li>" + link + songNames;
+            // console.log(returnedSongs[i].url);
 
-        $('#song-list').append(songList);
-
-    };
-
- });
+            $('#song-list').append(songList);
+        };
+    });
 
 
 
-// Call to grab event data
-  $.ajax({
-  url: EventQueryURL,
-  method: "GET"
+
+
+    // Creates AJAX call for Event Info (JSON: EventQueryURL)
+    $.ajax({
+    url: EventQueryURL,
+    method: "GET"
         }).done(function(response) {
 
         if (response.page.totalElements === 0) {
-                eventName, eventDate, eventTime, eventZone, eventStatus, eventPrice, eventVenue, eventCity, eventState, eventURL = "Null";
-                $('#tour-list').text("They're not on tour right now :(");
-            } else {
+            eventName, eventDate, eventTime, eventZone, eventStatus, eventPrice, eventVenue, eventCity, eventState, eventURL = "Null";
+            $('#tour-list').text("They're not on tour right now :(");
+        } else {
         // Variables defined key:values we need for Event Info (JSON: EventQueryURL)
         var eventName, eventDate, eventTime, eventZone, eventStatus, eventPrice, eventVenue, eventCity, eventState, eventURL;
- 
+
         // eventObject condenses response string
         var eventObject = response._embedded.events;
 
@@ -164,29 +176,30 @@ function search() {
         var bandObject;
         var nameKey;
 
-    // Function to check to see if child key:value exists in response
-    function checkKey(bandObject, nameKey){
+        // Function to check to see if child key:value exists in response
+        function checkKey(bandObject, nameKey){
 
-        if(bandObject.hasOwnProperty(nameKey)){
-            return bandObject[nameKey];
-        } else {
-            return null;
+            if(bandObject.hasOwnProperty(nameKey)){
+                return bandObject[nameKey];
+            } else {
+                return null;
 
+            }
         }
-    }
 
-    // Loop through event object (eventObject) to print all of the 
-    // key event data (date, location, city, price, etc)
-    for (var i = 0; i < numberOfEvents; i++){
+        // Loop through event object (eventObject) to print all of the 
+        // key event data (date, location, city, price, etc)
+        for (var i = 0; i < numberOfEvents; i++){
 
-        eventName   = checkKey(eventObject[i], "name");
-        eventDate   = checkKey(eventObject[i].dates.start, "localDate");            
-        eventTime   = checkKey(eventObject[i].dates.start, "localTime");
-        eventZone   = checkKey(eventObject[i].dates, "timezone");
-        eventStatus = checkKey(eventObject[i].dates.status, "code");
-        eventVenue  = checkKey(eventObject[i]._embedded.venues[0], "name");
-        eventCity   = checkKey(eventObject[i]._embedded.venues[0].city, "name");
-        eventURL    = checkKey(eventObject[i], "url");
+            // Define key event variables for display
+            eventName   = checkKey(eventObject[i], "name");
+            eventDate   = checkKey(eventObject[i].dates.start, "localDate");            
+            eventTime   = checkKey(eventObject[i].dates.start, "localTime");
+            eventZone   = checkKey(eventObject[i].dates, "timezone");
+            eventStatus = checkKey(eventObject[i].dates.status, "code");
+            eventVenue  = checkKey(eventObject[i]._embedded.venues[0], "name");
+            eventCity   = checkKey(eventObject[i]._embedded.venues[0].city, "name");
+            eventURL    = checkKey(eventObject[i], "url");
 
             // checkKey does not work for eventPrice...
             // Required to have a separate/custom checkKey conditional because
@@ -211,6 +224,7 @@ function search() {
                 eventState = " Null";
             }            
 
+            // Write event info to <div>
             var eventLink = "<a" + " target=" + "'_blank'" + "href=" + eventURL + ">";
             var eventInfo = eventName + " | " + eventDate + " | " + eventCity + " | " + eventState;
             var eventList = "<li>" + eventLink + eventInfo;
@@ -220,6 +234,7 @@ function search() {
                 $('#tour-list').text("They're not on tour right now :(")
             }
 
+            // Write event info to console.log
             console.log("Event Number: "        + i);
             console.log("Event Name: "          + eventName);
             console.log("Event Date: "          + eventDate);
@@ -232,17 +247,12 @@ function search() {
             console.log("Event State: "         + eventState);
             console.log("Event URL: "           + eventURL);
             console.log("--------------------------");
-            };
+            }
         }
-        }); 
+
+    });
 
 };
  
- // Creates AJAX call for Event Info (JSON: EventQueryURL)
 
- // Notes for 11/11/17
- // 1. Attach Audio Links to Song list
- // 2. Clean up page on load
- // 3. Introduce additional functionality (discuss with team)
- // 4. Event List
- // 5. Begin work on database storage
+ 
